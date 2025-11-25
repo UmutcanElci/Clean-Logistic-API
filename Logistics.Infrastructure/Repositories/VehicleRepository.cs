dadd589276230ec68c364c77eb9b957bf91edc88
Umutcan Elci <umutcanelci@yaani.com>
2025-11-25 18:30:17 +0300

    Not finished - Some of the Vehicle Dtos added - repo interfaces added - Repository finished

---
+++ Logistics.Infrastructure/Repositories/VehicleRepository.cs
@@ -0,0 +1,53 @@
+namespace Logistics.Infrastructure.Repositories;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Logistics.Application.Interfaces;
+using Logistics.Domain;
+using Microsoft.EntityFrameworkCore;
+
+public class VehicleRepository : IVehicleRepository
+{
+    private readonly LogisticsDbContext _context;
+
+    public VehicleRepository(LogisticsDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Vehicle> CreateAsync(Vehicle vehicle)
+    {
+        await _context.AddAsync(vehicle);
+        await _context.SaveChangesAsync();
+
+        return vehicle;
+    }
+
+    public async Task DeleteAsync(Guid vehicleId)
+    {
+        var vehicleToDelete = await GetByIdAsync(vehicleId);
+        _context.Vehicles.Remove(vehicleToDelete);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IReadOnlyList<Vehicle>> GetAllAsync()
+    {
+        return await _context.Vehicles
+          .Include(v => v.CurrentLocation)
+          .ToListAsync();
+    }
+
+    public async Task<Vehicle?> GetByIdAsync(Guid vehicleId)
+    {
+        return await _context.Vehicles
+          .Include(v => v.CurrentLocation)
+          .FirstOrDefaultAsync(o => o.Id == vehicleId);
+    }
+
+    public async Task UpdateAsync(Vehicle vehicle)
+    {
+        _context.Update(vehicle);
+        await _context.SaveChangesAsync();
+    }
+}
