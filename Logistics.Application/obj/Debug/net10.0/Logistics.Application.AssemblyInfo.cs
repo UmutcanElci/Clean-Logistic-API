5c327996dad0ecfd102534cbed60b81653825389
Umutcan Elci <umutcanelci@yaani.com>
2025-12-05 00:58:13 +0300

    OrderController done - now we create a test case for Order Controller

--- Logistics.Application/obj/Debug/net10.0/Logistics.Application.AssemblyInfo.cs
+++ Logistics.Application/obj/Debug/net10.0/Logistics.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7056987d65036a97baa9f5da244cf48883814d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eef9f351482517046fd4d967d90d77c54289b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Logistics.Web/Controllers/OrdersController.cs
@@ -0,0 +1,74 @@
+using Logistics.Application.DTOs;
+using Logistics.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logistics.Web.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly OrderService _service;
+
+    public OrdersController(OrderService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetOrderById(Guid id)
+    {
+        var order = await _service.GetOrderByIdAsync(id);
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderDto createOrder)
+    {
+        var createdOrder = await _service.CreateOrderAsync(createOrder);
+
+        return CreatedAtAction(nameof(GetOrderById), new { id = createdOrder.Id }, createdOrder);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllOrders()
+    {
+        var orders = await _service.GetAllOrderAsync();
+
+        return Ok(orders);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateOrder(Guid id, [FromBody] UpdateOrderDto orderDto)
+    {
+        if (id != orderDto.OrderId)
+        {
+            return BadRequest("Id Mismatch!");
+        }
+
+        try
+        {
+            await _service.UpdateOrderAsync(orderDto);
+        }
+        catch (ArgumentException ex)
+        {
+            return NotFound(ex.Message);
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteOrder(Guid id)
+    {
+        await _service.DeleteOrderAsync(id);
+
+        return NoContent();
+    }
+}
