7a50095f99fa61beb85f879a505aacf1d88aaeb4
Umutcan Elci <umutcanelci@yaani.com>
2025-12-09 01:13:23 +0300

    Starting the Warehouse Tests

--- Logistics.Web/obj/Debug/net10.0/Logistics.Web.AssemblyInfo.cs
+++ Logistics.Web/obj/Debug/net10.0/Logistics.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6e2ecae4c8993f345d1dbdb2cb9d0fb583af14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52db893fe2ab7d948b69c28334bdec5ee20d7f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
