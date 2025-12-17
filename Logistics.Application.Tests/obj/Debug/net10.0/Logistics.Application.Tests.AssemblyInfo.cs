da4d729b55bc9a679537b5e23dcbc8a2ec865325
Umutcan Elci <umutcanelci.me@gmail.com>
2025-12-17 22:39:05 +0300

    Delete fix Update added also need to look again later

--- Logistics.Application.Tests/obj/Debug/net10.0/Logistics.Application.Tests.AssemblyInfo.cs
+++ Logistics.Application.Tests/obj/Debug/net10.0/Logistics.Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfb720294c4a0d2d33fed92c9b8ea011a466406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0a14984aef8b0d189bd6db81f3a0f7ce1b0f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Logistics.Infrastructure/obj/Debug/net10.0/Logistics.Infrastructure.AssemblyInfo.cs
+++ Logistics.Infrastructure/obj/Debug/net10.0/Logistics.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f84c8ab2cd2509f2f3e8b10667dc355ba6f3db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0a14984aef8b0d189bd6db81f3a0f7ce1b0f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
