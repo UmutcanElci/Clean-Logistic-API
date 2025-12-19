4c2ebc9ff55a19a7437e78e46dc3e294df810aa8
Umutcan Elci <umutcanelci@yaani.com>
2025-12-19 13:50:59 +0300

    Warehouse UI and business core (Hub ) - Route create for Order

--- Logistics.Application/obj/Debug/net10.0/Logistics.Application.AssemblyInfo.cs
+++ Logistics.Application/obj/Debug/net10.0/Logistics.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5b9ea22c1eb568babb64dfd045bb1a31d4ea70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18101344d10f1d81e6b5626c642d7e5a51a41ed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
