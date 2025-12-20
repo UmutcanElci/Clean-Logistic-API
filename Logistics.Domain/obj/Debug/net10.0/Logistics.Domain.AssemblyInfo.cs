a5725c9b7ff6042c1b7b5310f6ad0f0fef911957
Umutcan Elci <umutcanelci.me@gmail.com>
2025-12-20 13:55:07 +0300

    About route about Hub try somethying now working will look again

--- Logistics.Domain/obj/Debug/net10.0/Logistics.Domain.AssemblyInfo.cs
+++ Logistics.Domain/obj/Debug/net10.0/Logistics.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logistics.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18101344d10f1d81e6b5626c642d7e5a51a41ed4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2ebc9ff55a19a7437e78e46dc3e294df810aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logistics.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logistics.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
